c4843f2384eb660461171ecf59933ddbae61e0ce
elliot <elliot@DESKTOP-F6M9L8G>
2021-04-22 23:17:29 +0100

    first commit

---
+++ todo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace todo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtNew = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(205, 48);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add New";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtNew
+            // 
+            this.txtNew.Location = new System.Drawing.Point(180, 22);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.Size = new System.Drawing.Size(138, 20);
+            this.txtNew.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 450);
+            this.Controls.Add(this.txtNew);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtNew;
+    }
+}
+
---
+++ todo/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace todo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("saveData/todolist.txt"))
+            {
+                File.Create("saveData/todolist.txt");
+            }
+            string[] lines = File.ReadAllLines("saveData/todolist.txt");
+            
+                TextBox[] textBox = new TextBox[lines.Length];
+                CheckBox[] checkBox = new CheckBox[lines.Length];
+                int checkBoxX, checkBoxY, textboxX, textboxY;
+
+                checkBoxX = 20;
+                checkBoxY = 20;
+                textboxX = 50;
+                textboxY = 20;
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    textBox[i] = new TextBox();
+                    textBox[i].Name = i.ToString();
+                    textBox[i].Text = lines[i];
+                    textBox[i].Location = new Point(textboxX, textboxY);
+
+                    checkBox[i] = new CheckBox();
+                    checkBox[i].Name = i.ToString();
+                    checkBox[i].Location = new Point(checkBoxX, checkBoxY);
+
+                    checkBoxY += 25;
+                    textboxY += 25;
+                }
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    this.Controls.Add(textBox[i]);
+                    this.Controls.Add(checkBox[i]);
+                }
+
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            string[] lines = File.ReadAllLines("saveData/todolist.txt");
+           
+            foreach (Control c in this.Controls)
+            {
+                if (c is CheckBox)
+                {
+                    CheckBox chk = (CheckBox)c;
+                    if (chk.Checked)
+                    {
+                        int i = int.Parse(chk.Name);
+                        var foos = new List<string>(lines);
+                        foos.RemoveAt(i);
+                        lines = foos.ToArray();
+                    }
+                }
+            }
+            File.WriteAllLines("saveData/todolist.txt", lines);
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("saveData/todolist.txt");
+            var list = new List<string>(lines);
+            list.Add(txtNew.Text);
+            File.WriteAllLines("saveData/todolist.txt",list);
+            Form1_Load(null, EventArgs.Empty);
+        }
+    }
+}
